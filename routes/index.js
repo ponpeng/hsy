28d12b3d68ca98d07ad5855b1d0775a54d47be55
ppkinnn <ponpon8@qq.com>
2018-06-06 23:31:50 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
   app.get('/', function (req, res) {
-    res.send('haoshouyi8.com is working')
+    res.send('haoshouyi8.com is working!!!')
   })
   app.use('/read', require('./read'))
 }
