204b8fc3429d2b59bfeea9335afedfc619c9754d
ppkinnn <ponpon8@qq.com>
2018-06-06 23:46:22 +0800

    1

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 const express = require('express')
 const routes = require('./routes')
 const config = require('config-lite')(__dirname)
+const path = require('path')
 
 const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 routes(app)
 
--- routes/read.js
+++ routes/read.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function(req, res, next){
-  res.send('read page')
+  //res.send('read page')
+  res.render('read')
 })
 
 module.exports = router
